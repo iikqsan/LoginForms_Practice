c26e69883a3f3d656a7aba8db74053628e7fa152
Prajak Udomsup <85482818+iikqsan@users.noreply.github.com>
2023-12-27 17:27:30 +0700

    create Object เพื่อไปแปลงไปเป็น JSON

--- script/form.js
+++ script/form.js
@@ -1,19 +1,35 @@
-const inputValue = document.querySelectorAll('.form-control')
-const genderValue = document.getElementsByName('genderRadio')
-
 function submitButton(){
-    for (let i = 0; i < inputValue.length ; i++){
-        console.log(inputValue[i].value)
+    let usernameDOM = document.querySelector('input[name=username]')
+    let passwordDOM = document.querySelector('input[name=password]')
+
+    let firstNameDOM = document.querySelector('input[name=firstname]')
+    let lastNameDOM = document.querySelector('input[name=lastname]')
+
+    let genderDOM = document.querySelector('input[name=gender]:checked')
+
+    let ageDOM = document.querySelector('input[name=age]')
+    let addressDOM = document.querySelector('input[name=address]')
+    let phoneNumberDOM = document.querySelector('input[name=phonenumber]')
+
+    //สร้าง object รับค่าเพื่อไปแปลงเป็น JSON ส่งข้อมูลให้กับ Backend
+    let userData = {
+        username: usernameDOM.value,
+        password: passwordDOM.value,
+        firstname: firstNameDOM.value,
+        lastName: lastNameDOM.value,
+        gender: genderDOM.value,
+        age: ageDOM.value,
+        address: addressDOM.value,
+        phonenumber: passwordDOM.value
     }
-    for (let i = 0; i < genderValue.length ; i++){
-        if(genderValue[i].checked){
-            console.log(genderValue[i].value)
-        }
-    }
+
+    console.log('submit', userData)
 }
 
+//ปุ่ม clear forms
+let clearDOM = document.querySelectorAll('.form-control')
 function clearButton(){
-    for (let i = 0; i < inputValue.length ; i++){
-        console.log(inputValue[i].value = "")
+    for (let i = 0; i < clearDOM.length ; i++){
+        console.log(clearDOM[i].value = "")
     }
 }
